c0a70f70ed547e541c623099d0e13833c9aa3e06
Koi-codeninjas <121695437+Koi-codeninjas@users.noreply.github.com>
2023-01-14 13:31:08 -0500

    Koi Meany Bird

---
+++ KoiMeanyBird2/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Game Controller Object for controlling the game")]
+    public GameController gameController;
+    [Header("Default Velocity")]
+    private float velocity= 5;
+    //bird physics
+    private Rigidbody2D rb;
+    //the height of the bird on the y axis
+    private float objectHeight;
+
+    void Start()
+    {
+        //GameController Component
+        gameController = GetComponent<GameController>();
+        //speed for game is playing at a slate
+        Time.timeScale = 1;
+
+        rb = GetComponent<Rigidbody2D>();
+        //object height equals the height of the spirte
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+
+
+    }
+
+    void Update()
+    {
+        if (UnityEngine.Input.GetMouseButtonDown(0))
+        {
+
+
+            //the bird will float up on y and down on y
+            rb.velocity = Vector2.up * velocity;
+
+        }
+    }
+}
---
+++ KoiMeanyBird2/Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //start function repeating every 4 seconds
+        InvokeRepeating("InstantiateObjects", 1f, 4f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(5, Random.Range(-height, height), 0);
+    }
+    //Object that we will attach to the script for the following object
+    [Header("Spikes object for controlling the game")]
+    public GameObject spikes;
+    //height of the sprite
+    [Header("Default height")]
+    public float height;
+    void InstantiateObjects()
+    {
+        Instantiate(spikes, transform.position, transform.rotation);
+    }
+
+}
